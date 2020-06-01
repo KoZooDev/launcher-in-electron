fdfd30f21cc911eacae00dc02cd9793a48b127dd
KoZoo <webdesigner.gov@gmail.com>
2020-06-01 15:33:37 -0300

    news add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 const remote = require('electron').remote;
 
-var windowTopBar = document.createElement('div')
-windowTopBar.style.width = "100%"
-windowTopBar.style.height = "20px"
-windowTopBar.style.backgroundColor = "transparent"
-windowTopBar.style.position = "absolute"
-windowTopBar.style.top = windowTopBar.style.left = 0
-windowTopBar.style.webkitAppRegion = "drag"
-document.body.appendChild(windowTopBar)
+
+        
+        var windowTopBar
+        windowTopBar = document.createElement('div')
+        windowTopBar.style.width = "100%"
+        windowTopBar.style.height = "20px"
+        windowTopBar.style.backgroundColor = "transparent"
+        windowTopBar.style.position = "absolute"
+        windowTopBar.style.top = windowTopBar.style.left = 0
+        windowTopBar.style.webkitAppRegion = "drag"
+         document.body.appendChild(windowTopBar)
+
+
 
 function closewindow() {
     var window = remote.getCurrentWindow();
