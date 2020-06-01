c3cf87573fe5d3e6c7537e99e6d5f770b3fe476b
KoZoo <>
2020-06-01 13:02:41 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const remote = require('electron').remote;
+
+var windowTopBar = document.createElement('div')
+windowTopBar.style.width = "100%"
+windowTopBar.style.height = "20px"
+windowTopBar.style.backgroundColor = "transparent"
+windowTopBar.style.position = "absolute"
+windowTopBar.style.top = windowTopBar.style.left = 0
+windowTopBar.style.webkitAppRegion = "drag"
+document.body.appendChild(windowTopBar)
+
+function closewindow() {
+    var window = remote.getCurrentWindow();
+    window.close();
+}
+
+function minimize() {
+    var window = remote.getCurrentWindow();
+    window.minimize();
+}
